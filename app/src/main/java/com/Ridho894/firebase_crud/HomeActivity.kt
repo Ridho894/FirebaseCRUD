04799e799d31002bcd7a4534d38b8096c127eea5
Muhamad Ridho Budikusuma <77140390+Ridho894@users.noreply.github.com>
2022-07-05 20:54:28 +0700

    layouting UI CRUD

--- app/src/main/java/com/Ridho894/firebase_crud/HomeActivity.kt
+++ app/src/main/java/com/Ridho894/firebase_crud/HomeActivity.kt
@@ -1,11 +1,18 @@
 package com.Ridho894.firebase_crud
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        val btnLogin = findViewById(R.id.addBtn) as Button
+
+        btnLogin.setOnClickListener {
+            startActivity(Intent(this, CreateActivity::class.java))
+        }
     }
 }
